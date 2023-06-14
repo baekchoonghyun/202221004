fa7d5b844ac6abe18dfc81e5f2842ff8557a18ef
choonghyun beak202221004 <136072782+baekchoonghyun@users.noreply.github.com>
2023-06-14 13:56:44 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,43 @@
 
+import numpy as np
+import matplotlib.pyplot as plt
+import control
+
+mine (void){
+num = [100]
+den = [1, 5, 6]  # (s+2)(s+3) = s^2 + 5s + 6
+G = control.TransferFunction(num, den)
+
+Gc = G * 1
+
+t = np.linspace(0, 10, 1000)
+
+u = np.ones_like(t)
+
+t, y = control.step_response(Gc, T=t, input=u)
+
+plt.plot(t, y)
+plt.xlabel('Time')
+plt.ylabel('Output')
+plt.title('Step Response')
+plt.grid(True)
+plt.show()
+
+omega, mag, phase = control.bode(Gc)
+
+plt.figure()
+plt.semilogx(omega, mag)
+plt.xlabel('Frequency (rad/s)')
+plt.ylabel('Magnitude (dB)')
+plt.title('Frequency Response')
+plt.grid(True)
+
+plt.figure()
+plt.semilogx(omega, phase)
+plt.xlabel('Frequency (rad/s)')
+plt.ylabel('Phase (degrees)')
+plt.title('Phase Response')
+plt.grid(True)
+
+plt.show()
+}
